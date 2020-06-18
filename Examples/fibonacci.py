7c7c0ed2ad856ad8187d4b3102b7cc9888761b0a
Fionagu <fiona.gu.1@lexisnexis.com>
2020-06-18 11:50:37 +0800

    deleteall

--- Examples/fibonacci.py
+++
@@ -1,6 +0,0 @@
-# 斐波那契数列
-def f(n):
-    if n ==1 or n ==2:
-        return 1
-    else:
-        return f(n-1)+f(n-2)
--- LA/getmoduleid.py
+++
@@ -1,7 +0,0 @@
-import xml.etree.cElementTree as ET
-
-tree = ET.parse('hk.xml')
-root = tree.getroot()
-
-for module in root.iter('Module'):
-    print(module.get('ID')+'\tSG '+module.get('NameKey')+'\t'+module.get('ModulePquid'))

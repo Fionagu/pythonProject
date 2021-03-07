744d5af50618b865c32206b5c62f09288237a0b1
Fionagu <fionagu531@163.com>
2021-03-07 20:13:41 +0800

    'archive'

--- temp.py
+++
@@ -1,7 +0,0 @@
-from os import listdir,path
-
-path_name = r'C:\Work\LATools\EDCGFiona\target\my\recognize\cert1_forms-my_baseline'
-f_list = listdir(path_name)
-# print(type(f_list))
-for index,f in enumerate(f_list):
-    print(index,f)
